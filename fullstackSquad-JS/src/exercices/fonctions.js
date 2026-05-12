dc783c08ef161c4031f5874ff3691b46fe39c573
Azograph <50628970+Azograph@users.noreply.github.com>
2026-05-12 16:16:33 +0200

    FEAT commentaire exercices tableaux

--- fullstackSquad-JS/src/exercices/fonctions.js
+++ fullstackSquad-JS/src/exercices/fonctions.js
@@ -1,22 +1,23 @@
-function test(number) {
-    let trenteTrois = 33 
-    console.log (trenteTrois + number)
+
+// Créer une focntion qui prend un nombre en paramètre.
+// La fonction doit afficher en console: 33+ le nombre reçu en paramètre.
+function test(number=1) { 
+    console.log (33 + number)
     }
 test(30);
 
-function test2(number) {
-    let trenteTrois = 33 
-    console.log ("33 + " + number)
-    }
-test2(30);
-
-
-function test3(number1, number2) {
+// Créer une fonction qui prend 2 nombres en paramètre.
+// Cette seconde fonction doit afficher en console l'addition des 2 nombres reçus en paramètre. 
+function test2(number1=1, number2=1) {
     console.log(number1 + number2)
 }
-test3(20,10);
+test2(20,10);
+test2(); // Cas d'erreur
 
-function test4(number1, number2) {
-    console.log(number1 + " + " + number2)
+// Fonction responsable :
+function test3(number1=1, number2=1) {
+    return number1 + number2
 }
-test4(20,10);
+console.log(test3(20,10));
+
+//
