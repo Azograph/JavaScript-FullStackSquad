13061c167932de028962912e52c0897b0a41ef85
Azograph <50628970+Azograph@users.noreply.github.com>
2026-05-12 15:52:23 +0200

    FEAT exercices

---
+++ fullstackSquad-JS/src/exercices/fonctions.js
@@ -0,0 +1,22 @@
+function test(number) {
+    let trenteTrois = 33 
+    console.log (trenteTrois + number)
+    }
+test(30);
+
+function test2(number) {
+    let trenteTrois = 33 
+    console.log ("33 + " + number)
+    }
+test2(30);
+
+
+function test3(number1, number2) {
+    console.log(number1 + number2)
+}
+test3(20,10);
+
+function test4(number1, number2) {
+    console.log(number1 + " + " + number2)
+}
+test4(20,10);
--- fullstackSquad-JS/src/exercices/tableaux.js
+++ fullstackSquad-JS/src/exercices/tableaux.js
@@ -6,3 +6,9 @@ let tabUser = [nom, age, passion];
 console.log (tabUser);
 console.log (tabUser[2]);
 console.log (tabUser[2][0]);
+
+tabUser.push("Ajout avec push", 2);
+console.log(tabUser);
+
+tabUser.pop(); // Suppression du dernier élément du tableau
+console.log(tabUser);
