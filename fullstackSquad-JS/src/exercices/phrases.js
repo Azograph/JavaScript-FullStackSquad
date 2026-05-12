3d6e7dc03cbcd3f12a8b450f09d303494e0a80e6
Azograph <50628970+Azograph@users.noreply.github.com>
2026-05-12 12:12:40 +0200

    FEAT exercices

--- fullstackSquad-JS/src/exercices/phrases.js
+++ fullstackSquad-JS/src/exercices/phrases.js
@@ -1,11 +1,17 @@
 let prenom = "Lana";
 let adresse = "59 rue de la petite provence";
 let commande = "Margarita";
+let ingreMargarita = new Object();
+ingreMargarita.base = "tomate"
+ingreMargarita.fromageUn = "mozzarella"
+ingreMargarita.top = "olive noir"
 let tempLivraison = 25;
-let dateJour = ["12","05","2026"];
 
-console.log (`Le ${dateJour} 
-    Bonjour ${prenom}, votre pizza: ${commande} est en cours de préparation. 
+const date = new Date().toLocaleDateString('fr-FR');
+
+
+console.log (`Le ${date}
+    Bonjour ${prenom}, votre pizza: ${commande}, base ${ingreMargarita.base},  ${ingreMargarita.fromageUn} et ${ingreMargarita.top} est en cours de préparation. 
     Elle vous sera livrée d'ici ${tempLivraison} minutes au ${adresse}.
     Merci d'avoir commandé chez la pizzaria "Rafinata"
     ---
---
+++ fullstackSquad-JS/src/exercices/tableaux.js
@@ -0,0 +1,8 @@
+let nom = "Gaston";
+let age = 75;
+let passion = ["pêche", "scrable"];
+let tabUser = [nom, age, passion];
+
+console.log (tabUser);
+console.log (tabUser[2]);
+console.log (tabUser[2][0]);
