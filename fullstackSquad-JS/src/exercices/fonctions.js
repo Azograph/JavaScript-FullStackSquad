4d7949f4112c09d5df4c6f64ecb53aeff6f6328c
Azograph <50628970+Azograph@users.noreply.github.com>
2026-05-13 09:38:57 +0200

    FEAT exercices

--- fullstackSquad-JS/src/exercices/fonctions.js
+++ fullstackSquad-JS/src/exercices/fonctions.js
@@ -1,68 +1,86 @@
 
-// Créer une focntion qui prend un nombre en paramètre.
-// La fonction doit afficher en console: 33+ le nombre reçu en paramètre.
-function test(number=1) { 
-    console.log (33 + number)
-    }
-test(30);
+    // // Créer une focntion qui prend un nombre en paramètre.
+    // // La fonction doit afficher en console: 33+ le nombre reçu en paramètre.
+    // function test(number=1) { 
+    //     console.log (33 + number)
+    //     }
+    // test(30);
 
-// Créer une fonction qui prend 2 nombres en paramètre.
-// Cette seconde fonction doit afficher en console l'addition des 2 nombres reçus en paramètre. 
-function test2(number1=1, number2=1) {
-    console.log(number1 + number2)
-}
-test2(20,10);
-test2(); // Cas d'erreur
+    // // Créer une fonction qui prend 2 nombres en paramètre.
+    // // Cette seconde fonction doit afficher en console l'addition des 2 nombres reçus en paramètre. 
+    // function test2(number1=1, number2=1) {
+    //     console.log(number1 + number2)
+    // }
+    // test2(20,10);
+    // test2(); // Cas d'erreur
 
-// Fonction responsable :
-function test3(number1=1, number2=1) {
-    return number1 + number2
-}
-console.log(test3(20,10));
+    // // Fonction responsable :
+    // function test3(number1=1, number2=1) {
+    //     return number1 + number2
+    // }
+    // console.log(test3(20,10));
 
 // CORRECTION 
 
-function additionner(nombre=0) {
-    console.log(33 + nombre);
-}
+// function additionner(nombre=0) {
+//     console.log(33 + nombre);
+// }
 
-additionner(2);
+// additionner(2);
 
-function additionner2(firstNumber=0, secondNumber=0) {
-    console.log(firstNumber + secondNumber);
-}
-additionner2(2, 3);
+// function additionner2(firstNumber=0, secondNumber=0) {
+//     console.log(firstNumber + secondNumber);
+// }
+// additionner2(2, 3);
 
-//! Pour mieux séparer la responsabilité de la fonction,on va dire que notre fonction doit retourner un résultat.
-//! On fait l'affichage dans un autre endroit.(console.log)
-//! On va aussi utiliser des paramètres par défaut pour éviter les erreurs de type "undefined".
+// //! Pour mieux séparer la responsabilité de la fonction,on va dire que notre fonction doit retourner un résultat.
+// //! On fait l'affichage dans un autre endroit.(console.log)
+// //! On va aussi utiliser des paramètres par défaut pour éviter les erreurs de type "undefined".
 
-function additionnerOpti(firstNumber=0, secondNumber=0) {
-    return firstNumber + secondNumber;
-}
-console.log(additionnerOpti(2, 3));
+// function additionnerOpti(firstNumber=0, secondNumber=0) {
+//     return firstNumber + secondNumber;
+// }
+// console.log(additionnerOpti(2, 3));
 
-// Ambiance fonction fléchée
-const additionnerFleche = (firstNumber=0, secondNumber=0) => {
-    return firstNumber + secondNumber;
-}
-console.log(additionnerFleche(2, 3));
+// // Ambiance fonction fléchée
+// const additionnerFleche = (firstNumber=0, secondNumber=0) => {
+//     return firstNumber + secondNumber;
+// }
+// console.log(additionnerFleche(2, 3));
 
-// Ambiance fonction fléchée optimisée
-const additionnerFlecheOpti = (firstNumber=0, secondNumber=0) => firstNumber + secondNumber;
-console.log(additionnerFlecheOpti(2, 3));
+// // Ambiance fonction fléchée optimisée
+// const additionnerFlecheOpti = (firstNumber=0, secondNumber=0) => firstNumber + secondNumber;
+// console.log(additionnerFlecheOpti(2, 3));
 
-// Ambiance fonction fléchée optimisée avec paramètre par défaut
-const additionnerFlecheOptiDefault = (firstNumber=0, secondNumber=0) => firstNumber + secondNumber;
-console.log(additionnerFlecheOptiDefault(2, 3));
+// // Ambiance fonction fléchée optimisée avec paramètre par défaut
+// const additionnerFlecheOptiDefault = (firstNumber=0, secondNumber=0) => firstNumber + secondNumber;
+// console.log(additionnerFlecheOptiDefault(2, 3));
 
 
-//! Fonction sécurisée pour éviter les erreurs de type "undefined".
-//! Avec gestion d'erreur.
-function additionnerOptiSécurisée(firstNumber=0, secondNumber=0) {
-    if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
-        throw new Error('Les paramètres doivent être des nombres');
+// //! Fonction sécurisée pour éviter les erreurs de type "undefined".
+// //! Avec gestion d'erreur.
+// function additionnerOptiSécurisée(firstNumber=0, secondNumber=0) {
+//     if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+//         throw new Error('Les paramètres doivent être des nombres');
+//     }
+//     return firstNumber + secondNumber;
+// }
+// console.log(additionnerOptiSécurisée("azerty", 3));
+
+
+let notes = [0,3,4,6,18,19,4]
+
+function calculerMoyenne ([]) {
+    total= 0
+    somme= 0
+    moyenne= 0
+    for (const element of notes) {
+        somme += element 
+        total +=1
     }
-    return firstNumber + secondNumber;
+    moyenne = somme / total
+    return moyenne
 }
-console.log(additionnerOptiSécurisée("azerty", 3));
+
+let resultat = calculerMoyenne (notes)
+console.log (resultat)
